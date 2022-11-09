c03437155ab34f15671342cc9a90d73a81c8f539
Dylan <dylflanagan@gmail.com>
2022-11-08 17:43:20 -0800

    Test written for books detail return

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -13,6 +13,15 @@ describe('books routes', () => {
     expect(res.body.length).toEqual(7);
   });
 
+  it('should return a book detail with authors', async () => {
+    const res = await request(app).get('/books/1');
+    expect(res.body).toEqual({
+      title: expect.any(String),
+      released: expect.any(Number),
+      authors: expect.any(Array),
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
